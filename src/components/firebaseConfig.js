c5aad0efd15e0e51a002cd16f584fcfe35b51454
unknown <Rezerbiw12>
2018-10-03 15:57:03 +0700

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default { 
+  apiKey: "AIzaSyCntbTptwRbqRDLjucGlYIPh15pGTrKyTg",
+  authDomain: "webapp2-211a8.firebaseapp.com",
+  databaseURL: "https://webapp2-211a8.firebaseio.com",
+  projectId: "webapp2-211a8",
+  storageBucket: "webapp2-211a8.appspot.com",
+  messagingSenderId: "123330568290"
+}
